0d132ff0b57d378cb2975f7a6fc358545fd15b24
KarthikaIndian <karthika.indian12@gmail.com>
2022-08-03 16:22:18 +0530

    for loop program array list

---
+++ index.js
@@ -0,0 +1,85 @@
+// Array value 19 how many times printed
+// let x = [19, 22, 18, 19, 16, 18, 19, 21, 24];
+
+// var sum=0;
+// for ( i=0; i<=x.length; i++) {
+           
+
+//   if (x[i] == 19 ) {
+//      sum++;
+
+//     }
+
+// }
+//  console.log(sum); 
+
+
+
+// Array value 19 which position printed
+
+// let x = [19, 22, 18, 19, 16, 18, 19, 21, 24];
+
+// let sum=0;
+// for ( i=0; i<=x.length; i++) {
+//     if (x[i]==19) {       
+
+//         console.log(i); 
+
+ 
+//     }
+   
+// }
+
+//  500 to 700 devided 8 reminder 0 only printed
+
+//    for(let i=500;i<=700; i++){
+//     if(i%8==3){
+//         console.log(i);
+//     }
+//    }
+
+
+// 700 to 900 divide 3,5,6 reminder 0 only printed 
+
+// for(i=700; i<=900;i++){
+//     if(i%3==0 && i%5==0 && i%6==0){
+//         console.log(i);
+//     }
+// }
+
+// Array object starting A letter name only printed
+
+// let students=["akaran","aravindhan","karthika","mounika","karnan","ajay","sanjay","anu","ahalya","samy"];
+// for(let i=0;i<=students.length;i++){
+//     if(students[i].indexOf("a")==0){
+//     console.log(students[i])
+// }
+// }
+
+// Array object Ending letter y only printed 
+
+
+// let students=["akaran","aravindhan","karthika","mounika","karnan","ajay","sanjay","anu","ahalya","aakshay","samy"];
+// for(let i=0;i<=students.length;i++){
+   
+// let a=students[i].slice(students[i].length-1)
+//     if(a=="y"){
+// console.log(students[i])
+//     }
+
+// }
+
+
+// Array object A start letter and Y end letter Only printed
+
+
+// let students=["akaran","aravindhan","karthika","mounika","karnan","ajay","sanjay","anu","ahalya","samy","aakshay","pandy"];
+// for(let i=0;i<=students.length;i++){
+   
+// let a=students[i].slice(students[i].length-1)
+
+//     if(a=="y" && students[i].indexOf("a")==0 ){
+// console.log(students[i])
+//     }
+
+// }
